a7ed2de8d61cd7d7320129914771c76d2a907db0
ZitangRong <zitangrong@gmail@org>
2018-09-21 15:33:50 -0700

    control test drive train

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CeciliaDrive.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class CeciliaDrive extends OpMode {
+
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+
+    public void init(){
+        leftMotor=hardwareMap.dcMotor.get("left_motor");
+        rightMotor=hardwareMap.dcMotor.get("right_motor");
+    }
+
+    public void loop(){
+        leftMotor.setPower(gamepad1.left_stick_y);
+        rightMotor.setPower(gamepad1.right_stick_y);
+    }
+}
