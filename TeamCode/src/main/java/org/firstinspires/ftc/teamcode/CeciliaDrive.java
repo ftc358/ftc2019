9db7082f2541fe9572a511636b6656da7cb08424
ZitangRong <zitangrong@gmail@org>
2018-10-01 15:53:21 -0700

    TeleOp//Oct 1st

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CeciliaDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CeciliaDrive.java
@@ -20,36 +20,5 @@ public class CeciliaDrive extends OpMode {
         leftMotor.setPower(gamepad1.left_stick_y);
         rightMotor.setPower(-gamepad1.right_stick_y);
     }
-
-    /*DigitalChannel digitalTouch;  // Hardware Device Object
-
-    @Override
-
-    public void runOpMode(){
-
-        // get a reference to our digitalTouch object.
-        digitalTouch = hardwareMap.get(DigitalChannel.class, "sensor_digital");
-
-        // set the digital channel to input.
-        digitalTouch.setMode(DigitalChannel.Mode.INPUT);
-
-        // wait for the start button to be pressed.
-        waitForStart();
-
-        // while the op mode is active, loop and read the light levels.
-        // Note we use opModeIsActive() as our loop condition because it is an interruptible method.
-        while (opModeIsActive()) {
-
-            // send the info back to driver station using telemetry function.
-            // if the digital channel returns true it's HIGH and the button is unpressed.
-            if (digitalTouch.getState() == true) {
-                telemetry.addData("Digital Touch", "Is Not Pressed");
-            } else {
-                telemetry.addData("Digital Touch", "Is Pressed");
-            }
-
-            telemetry.update();
-        }
-    }
-    */
+    //back of the motor facing right: positive
 }
