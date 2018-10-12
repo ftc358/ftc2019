440ec97c2e1809d0e4e84125aeabb63c66e7a283
billzheng <billzheng2003@gmail.com>
2018-10-12 15:05:13 -0700

    Comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exponential_Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exponential_Drive.java
@@ -15,6 +15,7 @@ public class Exponential_Drive extends OpMode{
     public void init() {
         leftMotor=hardwareMap.dcMotor.get("fM");
         rightMotor=hardwareMap.dcMotor.get("rM");
+        //Used for 2 motor drive//
     }
     double POWER = -1 * Range.clip(Math.max(Range.clip(Math.sqrt(Math.pow(gamepad1.left_stick_x, 2) + Math.pow(gamepad1.left_stick_y, 2)), -1, 1),
             Math.abs(gamepad1.right_stick_x)), -1, 1);
@@ -22,9 +23,11 @@ public class Exponential_Drive extends OpMode{
             Math.abs(gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x)),
             Math.max(Math.abs(gamepad1.left_stick_y),
                     Math.abs(gamepad1.right_stick_y)));
+    //More edit needed in order to run the motor efficiently//
 
     public void loop() {
         leftMotor.setPower(POWER * (gamepad1.left_stick_y)/maxPower);
         rightMotor.setPower(POWER * (gamepad1.right_stick_y)/maxPower);
     }
+    //This should run according to the function y=x^2//
 }
