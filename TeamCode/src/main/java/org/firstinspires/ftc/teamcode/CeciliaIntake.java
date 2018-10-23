ee8464553d3754cb37e192ec4c35d0ffd0be8b39
ZitangRong <zitangrong@gmail@org>
2018-10-23 15:28:36 -0700

    oct 23, 2018. adjusted value for cecilia intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CeciliaIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CeciliaIntake.java
@@ -19,14 +19,16 @@ public class CeciliaIntake extends OpMode {
     }
 
     public void loop(){
-        if(gamepad1.a){
-            leftMotor.setPower(0.3);
-            rightMotor.setPower(0.3);
+        if(gamepad1.b){
+            leftMotor.setPower(0.1);
+            rightMotor.setPower(0.1);
         }
         else{
-            if(gamepad1.b){
-                leftMotor.setPower(-0.3);
-                rightMotor.setPower(-0.3);
+            if(gamepad1.a){
+                leftMotor.setPower(-0.18);
+                rightMotor.setPower(-0.18);
+                leftMotor.setTargetPosition(336);
+                leftMotor.setTargetPosition(336);
             }
             else{
                 leftMotor.setPower(0);
