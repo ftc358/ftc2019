8b4e61ee4a3c9223bda177124572d37d5e8da2a2
Lawrence <DenouementX@users.noreply.github.com>
2018-10-24 16:10:29 -0700

    //bill

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rubber_band.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class Rubber_band extends OpMode{
+
+    DcMotor Band;
+
+    @Override
+
+    public void init() {
+        Band=hardwareMap.dcMotor.get("rB");
+    }
+    public void loop() {
+        if (gamepad1.left_bumper){
+            Band.setPower(0.75);
+        }
+        if (gamepad1.right_bumper){
+            Band.setPower(-0.75);
+        }
+
+    }
+}
