838cbc2fb7499d36eebf061fcaaef8e9441e5bdb
Lawrence <DenouementX@users.noreply.github.com>
2018-10-31 16:15:40 -0700

    official close red autonomous and teleop for team 358

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedC358.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class AutoRedC358 extends LinearOpMode{
+
+    enum state {
+        EXTEND,
+        JEWEL, STOP, RED, BLUE, TURN, BOX
+    }
+
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp358.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class TeleOp358 extends OpMode{
+
+    DcMotor lF;
+    DcMotor lB;
+    DcMotor rF;
+    DcMotor rB;
+
+    public void init(){
+
+        lF = hardwareMap.dcMotor.get("lF");
+        lB = hardwareMap.dcMotor.get("lB");
+        rF = hardwareMap.dcMotor.get("rF");
+        rB = hardwareMap.dcMotor.get("rB");
+
+        rF.setDirection(DcMotor.Direction.REVERSE);
+        rB.setDirection(DcMotor.Direction.REVERSE);
+
+    }
+
+    public void loop(){
+
+        lF.setPower(gamepad1.left_stick_y);
+        lB.setPower(gamepad1.left_stick_y);
+        rF.setPower(gamepad1.right_stick_y);
+        rB.setPower(gamepad1.right_stick_y);
+
+        // right trigger for precise movement
+        if (gamepad1.right_trigger>0.1f){
+
+            lF.setPower(((Math.abs(gamepad1.left_stick_y)*gamepad1.left_stick_y)/3)/2);
+            lB.setPower(((Math.abs(gamepad1.left_stick_y)*gamepad1.left_stick_y)/3)/2);
+            rF.setPower(((Math.abs(gamepad1.right_stick_y)*gamepad1.right_stick_y)/3)/2);
+            rB.setPower(((Math.abs(gamepad1.right_stick_y)*gamepad1.right_stick_y)/3)/2);
+
+        }
+    }
+}
