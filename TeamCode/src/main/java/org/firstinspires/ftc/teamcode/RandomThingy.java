1b0742822c113745aa2acda5739293418d43adf4
kevinwang <PhosphorusElement15@gmail.com>
2018-11-01 16:04:20 -0700

    OOF

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RandomThingy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RandomThingy.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
-public class TeleOp358 extends OpMode{
+public class RandomThingy extends OpMode{
 
     DcMotor lF;
     DcMotor lB;
