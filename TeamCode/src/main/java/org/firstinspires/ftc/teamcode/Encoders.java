73b4d7113540b1a2f037c9a5682ee76c7eeea72a
kevinwang <PhosphorusElement15@gmail.com>
2018-10-31 17:01:23 -0700

    Updated encoder code to work with 4 motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoders.java
@@ -5,37 +5,48 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class Encoders{
 
 
-    public static void Forward (DcMotor leftMotor, DcMotor rightMotor, double power, int distance) {
+    public static void Forward (DcMotor motor1, DcMotor motor2, DcMotor motor3, DcMotor motor4, double power, int distance) {
 
         //Reset Encoders
-        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor4.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         //Set to RUN_TO_POSITION mode
-        leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor4.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         //Set Target Position
-        leftMotor.setTargetPosition(distance);
-        rightMotor.setTargetPosition(distance);
+        motor1.setTargetPosition(distance);
+        motor2.setTargetPosition(distance);
+        motor3.setTargetPosition(distance);
+        motor4.setTargetPosition(distance);
 
         //Set Drive Power
-        leftMotor.setPower(power);
-        rightMotor.setPower(power);
+        motor1.setPower(power);
+        motor2.setPower(power);
+        motor3.setPower(power);
+        motor4.setPower(power);
 
-        while (leftMotor.isBusy() && rightMotor.isBusy())
+        while (motor1.isBusy() && motor2.isBusy() && motor3.isBusy() && motor4.isBusy())
         {
             //Wait Until Target Position is Reached
         }
 
         //Stop and Change Mode back to Normal
-        leftMotor.setPower(0);
-        rightMotor.setPower(0);
+        motor1.setPower(0);
+        motor2.setPower(0);
+        motor3.setPower(0);
+        motor4.setPower(0);
     }
 
     //negative distance to turn left; positive distance to turn right
 
-    public static void Turn (DcMotor leftMotor, DcMotor rightMotor, double power, int distance) {
+    /*
+       public static void Turn (DcMotor leftMotor, DcMotor rightMotor, double power, int distance) {
 
         //Reset Encoders
         leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -62,4 +73,5 @@ public class Encoders{
         leftMotor.setPower(0);
         rightMotor.setPower(0);
     }
+     */
 }
