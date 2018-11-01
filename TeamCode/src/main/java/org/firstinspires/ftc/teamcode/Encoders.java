bf6ae94ec6b623fe69bb656ba88a98d492104c69
Calvin Xu <1020806611@qq.com>
2018-11-01 16:07:18 -0700

    Encoder refractored

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoders.java
@@ -2,65 +2,83 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-public class Encoders{
+import java.util.ArrayList;
+
+public class Encoders {
 
 
-    public static void Forward (DcMotor leftMotor, DcMotor rightMotor, double power, int distance) {
+    public static void Forward(ArrayList<DcMotor> motorArray, double power, int distance) {
 
-        //Reset Encoders
-        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ArrayList<DcMotor> motors = new ArrayList<>();
 
-        //Set to RUN_TO_POSITION mode
-        leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        for (DcMotor motor : motorArray) {
 
-        //Set Target Position
-        leftMotor.setTargetPosition(distance);
-        rightMotor.setTargetPosition(distance);
+            //Reset Encoders
+            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            //Set to RUN_TO_POSITION mode
+            motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            //Set Target Position
+            motor.setTargetPosition(distance);
+            //Set Drive Power
+            motor.setPower(power);
 
-        //Set Drive Power
-        leftMotor.setPower(power);
-        rightMotor.setPower(power);
+            motors.add(motor);
+        }
 
-        while (leftMotor.isBusy() && rightMotor.isBusy())
-        {
+        while (allMotorStatus(motors)) {
             //Wait Until Target Position is Reached
         }
 
         //Stop and Change Mode back to Normal
-        leftMotor.setPower(0);
-        rightMotor.setPower(0);
+        stopAllMotors(motors);
     }
 
-    //negative distance to turn left; positive distance to turn right
+    public static void Turn(ArrayList<DcMotor> motorArray, double power, int distance) {
 
-    public static void Turn (DcMotor leftMotor, DcMotor rightMotor, double power, int distance) {
+        ArrayList<DcMotor> motors = new ArrayList<>();
+        int i = 1;
 
-        //Reset Encoders
-        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        for (DcMotor motor : motorArray) {
 
-        //Set to RUN_TO_POSITION mode
-        leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            //Reset Encoders
+            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            //Set to RUN_TO_POSITION mode
+            motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            //Set Target Position
+            //Negative distance to turn left; positive distance to turn right
+            if ((i % 2) == 0) {
+                motor.setTargetPosition(distance);
+            } else {
+                motor.setTargetPosition(-distance);
+            }
+            //Set Drive Power
+            motor.setPower(power);
 
-        //Set Target Position
-        leftMotor.setTargetPosition(distance);
-        rightMotor.setTargetPosition(-1*distance);
+            motors.add(motor);
 
-        //Set Drive Power
-        leftMotor.setPower(power);
-        rightMotor.setPower(power);
+            i++;
+        }
 
-        while (leftMotor.isBusy() && rightMotor.isBusy())
-        {
+        while (allMotorStatus(motors)) {
             //Wait Until Target Position is Reached
         }
 
         //Stop and Change Mode back to Normal
-        leftMotor.setPower(0);
-        rightMotor.setPower(0);
+        stopAllMotors(motors);
+    }
 
+    public static boolean allMotorStatus(ArrayList<DcMotor> motorList) {
+        for (DcMotor motor : motorList) {
+            if (motor.isBusy()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void stopAllMotors(ArrayList<DcMotor> motorList) {
+        for (DcMotor motor : motorList) {
+            motor.setPower(0);
+        }
     }
 }
