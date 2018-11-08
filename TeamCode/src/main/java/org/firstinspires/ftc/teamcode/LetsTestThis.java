9500abb1a744fe4a812cdc45bb070614d3800918
StoryScene <emmaqin0722@gmail.com>
2018-11-08 15:43:14 -0800

    --- -.-

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LetsTestThis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LetsTestThis.java
@@ -1,25 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-
-
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-
-
 import java.util.ArrayList;
-
-
-
-
-
-
-
 @Autonomous
-
 public class LetsTestThis extends LinearOpMode{
 
 
@@ -28,100 +14,92 @@ public class LetsTestThis extends LinearOpMode{
 
     DcMotor lB;
 
+
+
     DcMotor rF;
     DcMotor rB;
     state state358;
 
+
+
     enum state {
-
-
-
-
-
-
-
         EXTEND, DETECT, KNOCK, STOP
     }
+
     public void runOpMode() throws InterruptedException {
-
-
 
         lF = hardwareMap.dcMotor.get("lF");
 
+
+
         lB = hardwareMap.dcMotor.get("lB");
-
         rF = hardwareMap.dcMotor.get("rF");
-
         rB = hardwareMap.dcMotor.get("rB");
 
-
-
         rF.setDirection(DcMotor.Direction.REVERSE);
-
         rB.setDirection(DcMotor.Direction.REVERSE);
-
-
-
 //        ArrayList<DcMotor> motorArrayList = new ArrayList<DcMotor>();
 
 //        motorArrayList.add(lF);
-
 //        motorArrayList.add(lB);
-
-
-
-
-
-
-
 //        motorArrayList.add(rF);
 
+
+
 //        motorArrayList.add(rB);
+
+
+
         state358 = state.EXTEND;
+
         waitForStart();
 
         while (opModeIsActive()) {
+
+
+
             telemetry.addData("Going into state", state358);
+
             telemetry.update();
-
-
-
             switch(state358) {
                 case EXTEND:
-                    Encoders.Forward(lF, lB, rF, rB, 0.25, 1000);
 
+                    Encoders.Forward(lF, lB, rF, rB, 0.25, 1000);
                     state358 = state.DETECT;
                     break;
-                case DETECT:
 
+
+
+                case DETECT:
                     // do something
                     state358 = state.KNOCK;
+
                     break;
-
-
-
                 case KNOCK:
-
                     // do something
+
                     state358 = state.STOP;
                     break;
-
                 case STOP:
 
 
 
                     lF.setPower(0);
+
                     lB.setPower(0);
                     rF.setPower(0);
-
                     rB.setPower(0);
 
                     sleep(30000);
+
+
+
             }
         }
-
     }
 
 }
 
 //Wow
+// :( spaces
+//congrats
