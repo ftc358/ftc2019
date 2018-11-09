e50d02ebf304fdb9c77af89b43ea83fe2e45da72
Charlie Sun <csun@webb.org>
2018-11-09 15:13:20 -0800

    Created an Autonomous 359 and updated the Telop 359

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Archive/Sample_Vuforia_Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Archive/Sample_Vuforia_Autonomous.java
@@ -267,3 +267,29 @@ public class Sample_Vuforia_Autonomous extends LinearOpMode {
         }
     }
 }
+
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+void Homecoming(std::string &ans);
+
+        int main()
+        {
+        std::string answer;
+        do {
+        Date(answer);
+        } while (answer != "yes");
+        std::cout << "See you on Monday at 7?";	// final message
+        return 0;
+        }
+
+        void Date(std::string &ans)
+        {
+        std::cout << "Will you come out for a date with me? ";
+        std::cin >> ans;
+        std::transform(ans.begin(), ans.end(), ans.begin(), ::tolower);
+        if (ans != "yes")
+        std::cout << "****** Error, answer not accepted, try again!" << std::endl;
+        }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous359.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+public class Autonomous359 {
+    @Autonomous
+}
