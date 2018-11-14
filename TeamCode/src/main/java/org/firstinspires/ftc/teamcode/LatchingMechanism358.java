60f00a70c94b2cbdf134bd2e5c43bd699efb4b12
ZitangRong <zitangrong@gmail@org>
2018-11-13 16:13:57 -0800

    code for collecting

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LatchingMechanism358.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+
+public class LatchingMechanism358 extends OpMode{
+    DcMotor Motor;
+
+    public void init() {
+        Motor = hardwareMap.dcMotor.get("Motor");
+        Motor.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    public void loop() {
+        Motor.setPower(gamepad1.left_stick_y);
+    }
+}
