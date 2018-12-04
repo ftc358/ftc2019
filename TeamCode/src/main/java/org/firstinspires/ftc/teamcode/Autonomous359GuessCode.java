532d4858396d4d5ba79b7131c181e5ba2bac649f
Charlie Sun <csun@webb.org>
2018-12-03 16:29:47 -0800

    Deleted incorrect Auto 359 codes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous359GuessCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous359GuessCode.java
@@ -2,20 +2,16 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @Autonomous
-public class Autonomous359L1 extends LinearOpMode {
+public class Autonomous359GuessCode extends LinearOpMode {
 
     DcMotor leftMotor;
     DcMotor rightMotor;
     DcMotor leftLatch;
     DcMotor rightLatch;
-    DcMotor intakeRotation;
-    DcMotor drawerSlide;
-    CRServo intake;
 
     public void runOpMode() throws InterruptedException {
 
@@ -23,9 +19,6 @@ public class Autonomous359L1 extends LinearOpMode {
         rightMotor=hardwareMap.dcMotor.get("rM");
         leftLatch=hardwareMap.dcMotor.get("lL");
         rightLatch=hardwareMap.dcMotor.get("rL");
-        intakeRotation=hardwareMap.dcMotor.get("iR");
-        drawerSlide=hardwareMap.dcMotor.get("dS");
-        intake = hardwareMap.crservo.get("i");
 
         rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         rightLatch.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -34,11 +27,9 @@ public class Autonomous359L1 extends LinearOpMode {
 
         while (opModeIsActive()){
 
-            Encoders359.Forward(leftLatch,rightLatch,1,-3000);      //Move the robot down
+            //Move the robot down
             //Move the hook
-            Encoders359.Forward(leftMotor,rightMotor,0.3,5000);     //Move Forward to get rid of the hook
-            Encoders359.Turn(leftMotor,rightMotor,0.3,888);         //Turn toward the mineral
-            Encoders359.Forward(leftMotor,rightMotor,0.3,1000);     //Go forward and knock the mineral
+            Encoders359.Forward(leftMotor,rightMotor,0.25,4000);     //Knock the ball
         }
     }
 }
