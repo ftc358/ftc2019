2b62245e4b9467b4bcd8162388ade61158a0c828
Charlie Sun <csun@webb.org>
2018-12-03 16:26:18 -0800

    Teleop 359 update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoders359.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoders359.java
@@ -58,6 +58,29 @@ public class Encoders359{
         motor2.setPower(0);
     }
 
+    public static void Slide(DcMotor motor1, double power, int distance) {
+
+        //Reset Encoders
+        motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //Set to RUN_TO_POSITION mode
+        motor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+        //Set Target Position
+        motor1.setTargetPosition(distance);
+
+        //Set Drive Power
+        motor1.setPower(power);
+
+        while (motor1.isBusy()) {
+            //Wait Until Target Position is Reached
+        }
+
+        //Stop and Change Mode back to Normal
+        motor1.setPower(0);
+    }
+
     public static void Intake(DcMotor motor1, double power, int distance) {
 
         //Reset Encoders
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp359.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp359.java
@@ -2,72 +2,90 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp
-public class TeleOp359 extends OpMode{
+public class TeleOp359 extends OpMode
+{
 
     DcMotor leftMotor;
     DcMotor rightMotor;
     DcMotor leftLatch;
     DcMotor rightLatch;
-    DcMotor intakeRotation;
-    DcMotor drawerSlide;
-    CRServo intake;
+    DcMotor slideRotation;
+    DcMotor slideExtend;
+    DcMotor slideRetract;
 
-    public void init() {
-        leftMotor=hardwareMap.dcMotor.get("lM");
-        rightMotor=hardwareMap.dcMotor.get("rM");
-        leftLatch=hardwareMap.dcMotor.get("lL");
-        rightLatch=hardwareMap.dcMotor.get("rL");
-        intakeRotation=hardwareMap.dcMotor.get("iR");
-        drawerSlide=hardwareMap.dcMotor.get("dS");
-        intake = hardwareMap.crservo.get("i");
+    public void init()
+    {
+        leftMotor = hardwareMap.dcMotor.get("lM");
+        rightMotor = hardwareMap.dcMotor.get("rM");
+        leftLatch = hardwareMap.dcMotor.get("lL");
+        rightLatch = hardwareMap.dcMotor.get("rL");
 
         rightLatch.setDirection(DcMotorSimple.Direction.REVERSE);
         rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
-    public void loop() {
+    public void loop()
+    {
 
         leftMotor.setPower(Range.clip(((gamepad1.left_stick_y) * Math.abs(gamepad1.left_stick_y)), -1, 1));
         rightMotor.setPower(Range.clip(((gamepad1.right_stick_y) * Math.abs(gamepad1.right_stick_y)), -1, 1)); //let the chassis move according to the function y=x^2 or -x^2 depending on the value of x//
 
-        if (gamepad2.left_bumper) {
+        if (gamepad1.left_bumper)               //Latching Mechanism
+        {
             leftLatch.setPower(1);
             rightLatch.setPower(1);
         }
-        else if (gamepad2.right_bumper) {
+        else if (gamepad1.right_bumper)
+        {
             leftLatch.setPower(-1);
             rightLatch.setPower(-1);
         }
-        else {
+        else
+        {
             leftLatch.setPower(0);
             rightLatch.setPower(0);
         }
 
-        if (gamepad2.a) {
-            intake.setPower(1);
+
+
+        if (gamepad2.dpad_up)                   //Slide Rotation
+        {
+            slideRotation.setPower(0.3);
         }
-        else if (gamepad2.b) {
-            intake.setPower(-1);
+        else if (gamepad2.dpad_down)
+        {
+            slideRotation.setPower(-0.3);
         }
-        else {
-            intake.setPower(0);
+        else
+        {
+            slideRotation.setPower(0);
         }
 
-        if (gamepad2.dpad_up) {
-            intakeRotation.setPower(0.5);
+
+
+        if (gamepad2.left_bumper)                                   //Slide Extend
+        {
+            slideExtend.setPower(0.5);
         }
-        else if (gamepad2.dpad_down) {
-            intakeRotation.setPower(-0.5);
-        }
-        else {
-            intakeRotation.setPower(0);
+        else
+        {
+            slideExtend.setPower(0);
         }
 
+
+
+        if (gamepad2.right_bumper)                                   //Slide Retract
+        {
+            slideRetract.setPower(0.5);
+        }
+        else
+        {
+            slideRetract.setPower(0);
+        }
     }
 }
