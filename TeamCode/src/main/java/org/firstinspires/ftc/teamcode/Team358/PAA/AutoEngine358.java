20ba49d2f9547fa5e1d03b6b42f13579b2aa9585
Calvin Xu <1020806611@qq.com>
2018-12-25 19:11:14 +0800

    fix later

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team358/PAA/AutoEngine358.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team358/PAA/AutoEngine358.java
@@ -83,20 +83,21 @@ public abstract class AutoEngine358 extends Robot358Main {
     }
 
     public List<RobotPosition> findingTurns(List<RobotPosition> positions) {
-        RobotPosition lastPosition = positions.get(0);
+//        RobotPosition lastPosition = positions.get(0);
         List<RobotPosition> processedRoute = new ArrayList<>();
-        processedRoute.add(lastPosition);
-        positions.remove(0);
-        for (RobotPosition position : positions) {
-            if (!((position.x == lastPosition.x) || (position.y == lastPosition.y) ||
-                    ((position.x == (lastPosition.x + 1)) && (position.y == (lastPosition.y + 1))) ||
-                    ((position.x == (lastPosition.x + 1)) && (position.y == (lastPosition.y - 1))) ||
-                    ((position.x == (lastPosition.x - 1)) && (position.y == (lastPosition.y + 1))) ||
-                    ((position.x == (lastPosition.x - 1)) && (position.y == (lastPosition.y - 1))))) {
-                position.isTurn = true;
-                processedRoute.add(position);
-            }
-        }
+//        processedRoute.add(lastPosition);
+//        positions.remove(0);
+//        for (RobotPosition position : positions) {
+//            if (!((position.x == lastPosition.x) || (position.y == lastPosition.y) ||
+//                    ((position.x == (lastPosition.x + 1)) && (position.y == (lastPosition.y + 1))) ||
+//                    ((position.x == (lastPosition.x + 1)) && (position.y == (lastPosition.y - 1))) ||
+//                    ((position.x == (lastPosition.x - 1)) && (position.y == (lastPosition.y + 1))) ||
+//                    ((position.x == (lastPosition.x - 1)) && (position.y == (lastPosition.y - 1))))) {
+//                position.isTurn = true;
+//                processedRoute.add(position);
+//            }
+//        }
         return processedRoute;
+        //TODO: bad, fix
     }
 }
