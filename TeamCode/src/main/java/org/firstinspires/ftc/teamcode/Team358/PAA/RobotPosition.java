de0c21b627d74b2c5846758db703b11c17200308
Calvin Xu <1020806611@qq.com>
2018-12-25 10:48:22 +0800

    Headings according to datasheet? Vuforia in Robot358Main

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team358/PAA/RobotPosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team358/PAA/RobotPosition.java
@@ -25,22 +25,24 @@ public class RobotPosition {
      * 6 1 7
      */
 
-    //TODO: change to actual headings
+    //TODO: test actual headings
+    //I have no idea if it works. According to the datasheet the heading increases clockwise from 0 to 360, but I do not remember it that way
+    //Will have to test
 
     public double getRelativeHeading(RobotPosition lastPosition) {
-        int relativeHeading = 0;
+        double relativeHeading = 0;
 
         switch (this.x - lastPosition.x) {
             case 1:
                 switch (this.y - lastPosition.y) {
                     case 1:
-                        relativeHeading = 5;
+                        relativeHeading = 45;
                         break;
                     case 0:
-                        relativeHeading = 3;
+                        relativeHeading = 90;
                         break;
                     case -1:
-                        relativeHeading = 7;
+                        relativeHeading = 135;
                         break;
                     default:
                         throw new IllegalArgumentException("Positions not adjacent");
@@ -54,7 +56,7 @@ public class RobotPosition {
                     case 0:
                         throw new IllegalArgumentException("Is current position");
                     case -1:
-                        relativeHeading = 1;
+                        relativeHeading = 180;
                         break;
                     default:
                         throw new IllegalArgumentException("Positions not adjacent");
@@ -63,13 +65,14 @@ public class RobotPosition {
             case -1:
                 switch (this.y - lastPosition.y) {
                     case 1:
-                        relativeHeading = 4;
+                        relativeHeading = 315;
                         break;
                     case 0:
-                        relativeHeading = 2;
+                        relativeHeading = 270;
                         break;
                     case -1:
-                        relativeHeading = 6;break;
+                        relativeHeading = 225;
+                        break;
                     default:
                         throw new IllegalArgumentException("Positions not adjacent");
                 }
