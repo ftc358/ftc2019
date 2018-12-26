ea984d88a7c2e348f7c8a2766618ecf9842adb76
Calvin Xu <1020806611@qq.com>
2018-12-26 12:47:36 +0800

    Higher API level & source & target compatibility. We need to update phones to Nougat.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team358/PAA/AutoEngine358.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team358/PAA/AutoEngine358.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.util.RobotLog;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.lang.Math.abs;
 
@@ -83,21 +85,14 @@ public abstract class AutoEngine358 extends Robot358Main {
     }
 
 //    public List<RobotPosition> findingTurns(final List<RobotPosition> positions) {
-////        List<Point> points = new ArrayList<Point>();
-////        points.add(createPoint("A", 1, 0));
-////        points.add(createPoint("B", 1, 1));
-////        points.add(createPoint("C", 1, 2));
-////        points.add(createPoint("D", 2, 2));
-////        points.add(createPoint("E", 3, 1));
-////        points.add(createPoint("F", 4, 1));
-////        points.add(createPoint("G", 5, 1));
-////        points.add(createPoint("H", 5, 2));
 //
 //        List<Integer> indices = computeTurningPointIndices(positions);
 //        System.out.println("Turning points are at " + indices);
 //
-////        List<RobotPosition> turningPoints = Stream.of(indices).map(i -> positions.get(i))
-////                .collect(Collectors.toList());
+//        List<RobotPosition> turningPoints = Stream.of(indices).map(i -> positions.get(i))
+//                .collect(Collectors.toList());
+//
+//        return turningPoints;
 //
 ////        System.out.println("Collinear:");
 ////        indices.add(0, 0);
