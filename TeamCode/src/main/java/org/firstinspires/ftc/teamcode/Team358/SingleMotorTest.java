aab8adfbdfa7b3a37429d52234766387e28a323c
Calvin Xu <1020806611@qq.com>
2019-01-18 18:46:10 -0800

    misc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team358/SingleMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team358/SingleMotorTest.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.Team358;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp
+//@TeleOp
+@Autonomous
 public class SingleMotorTest extends LinearOpMode {
     DcMotor motor;
 
@@ -14,7 +16,20 @@ public class SingleMotorTest extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            motor.setPower(gamepad1.left_stick_y);
+//            motor.setPower(gamepad1.left_stick_y);
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motor.setTargetPosition(20);
+
+            motor.setPower(1);
+
+            while (motor.isBusy()) {
+                //Wait Until Target Position is Reached
+            }
+
+            //Stop and Change Mode back to Normal
+            motor.setPower(0);
         }
     }
 }
