363b3ab67577d0ae57655246df18e823650f52c3
Calvin Xu <1020806611@qq.com>
2019-01-25 14:44:04 -0800

    changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team358/Playground.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team358/Playground.java
@@ -10,23 +10,28 @@ public class Playground extends LinearOpMode {
     DcMotor motor2;
 
     public void runOpMode() throws InterruptedException {
-        motor1 = hardwareMap.dcMotor.get("latch");
+        motor1 = hardwareMap.dcMotor.get("lF");
 //        motor2 = hardwareMap.dcMotor.get("motor2");
 
         waitForStart();
 
         motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 //        motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 //        motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        motor1.setPower(0.5);
-//        motor2.setPower(0.5);
+        waitForStart();
 
-        while (opModeIsActive()) {
-            telemetry.addData("REV Encoder reading", motor1.getCurrentPosition());
-//            telemetry.addData("Tetrix Encoder reading", motor2.getCurrentPosition());
-            telemetry.update();
+        motor1.setTargetPosition(7000);
+
+        motor1.setPower(1.0);
+
+        while (opModeIsActive() && motor1.isBusy())
+        {
+//            telemetry.addData("encoder-fwd", motor1.getCurrentPosition() + "  busy=" + motor1.isBusy());
+//            telemetry.update();
         }
+
+        motor1.setPower(0.0);
     }
 }
