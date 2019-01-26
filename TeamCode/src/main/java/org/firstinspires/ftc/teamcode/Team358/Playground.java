6a3ec6d3ddd34054f6922bc992410a371c9b791e
Calvin Xu <1020806611@qq.com>
2019-01-25 16:05:05 -0800

    why not work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team358/Playground.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team358/Playground.java
@@ -9,8 +9,40 @@ public class Playground extends Robot358Main {
 
     public void runOpMode() throws InterruptedException {
 
+        initialize();
+
         waitForStart();
 
+        latchModule = new LatchModule(latch) {
+            @Override
+            public void run() {
+                super.run();
+                super.unlatch();
+                if (Thread.interrupted()) {
+                    telemetry.addData("Latch module operation interrupted", "!!!");
+                }
+            }
+
+            @Override
+            public void unlatch() {
+                latchMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+                latchMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                latchMotor.setTargetPosition(7000);
+
+                latchMotor.setPower(1);
+
+                while (latchMotor.isBusy()) {
+                    //Wait Until Target Position is Reached
+                    telemetry.addData("Current Position", latch.getCurrentPosition());
+                }
+
+                latchMotor.setPower(0);
+            }
+        };
+
         latchModule.start();
+
     }
 }
