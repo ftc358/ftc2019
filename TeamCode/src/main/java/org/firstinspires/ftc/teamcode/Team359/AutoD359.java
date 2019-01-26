89e9cffd34d1581f4703d136d621e38db5b746dd
Charlie Sun <csun@webb.org>
2019-01-25 21:31:18 -0800

    update 359

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team359/AutoD359.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team359/AutoD359.java
@@ -98,11 +98,11 @@ public class AutoD359 extends LinearOpMode {
                 case DRIVE:
                     if (detected == 1) {
                         Encoders359.Turn(leftMotor,rightMotor,0.5,-1000);
-                        Encoders359.Forward(leftMotor,rightMotor,0.5,1000);
+                        Encoders359.Forward(leftMotor,rightMotor,0.5,2000);
                         Encoders359.Intake(Rotation,0.5,-1000);
                         Encoders359.Intake(Rotation,0.5,1000);
                         Encoders359.Forward(leftMotor,rightMotor,0.5,-3000);
-                        Encoders359.Turn(leftMotor,rightMotor,0.5,3000);
+                        Encoders359.Turn(leftMotor,rightMotor,0.5,2000);
                         Encoders359.Forward(leftMotor,rightMotor,0.5,5000);
                     } else if (detected == 2) {
                         Encoders359.Intake(Rotation,0.5,-1000);
