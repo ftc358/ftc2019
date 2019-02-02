d3bbdea62abbc5f36ae88addc2440a2d55ef3eae
Calvin Xu <1020806611@qq.com>
2019-02-01 21:58:29 -0800

    misc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team358/PAAPlayground.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team358/PAAPlayground.java
@@ -14,33 +14,37 @@ public class PAAPlayground extends AutoEngine358 {
 
     public void runOpMode() throws InterruptedException {
 
-        initialize(new RobotPosition(12, 12, 45));
+        initialize(new RobotPosition(32, 0, 0));
 
-        autoRoute.add(new RobotPosition(12, 12, 45));
-        autoRoute.add(new RobotPosition(13, 12));
-        autoRoute.add(new RobotPosition(14, 12));
-        autoRoute.add(new RobotPosition(15, 12));
-        autoRoute.add(new RobotPosition(16, 12));
-        autoRoute.add(new RobotPosition(17, 12));
-        autoRoute.add(new RobotPosition(18, 12));
-        autoRoute.add(new RobotPosition(19, 12));
-        autoRoute.add(new RobotPosition(20, 12));
-        autoRoute.add(new RobotPosition(20, 13));
-        autoRoute.add(new RobotPosition(20, 14));
-        autoRoute.add(new RobotPosition(20, 15));
-        autoRoute.add(new RobotPosition(20, 16));
-        autoRoute.add(new RobotPosition(20, 17));
-        autoRoute.add(new RobotPosition(20, 18));
-        autoRoute.add(new RobotPosition(20, 19));
-        autoRoute.add(new RobotPosition(20, 20));
-        autoRoute.add(new RobotPosition(19, 19));
-        autoRoute.add(new RobotPosition(18, 18));
-        autoRoute.add(new RobotPosition(17, 17));
-        autoRoute.add(new RobotPosition(16, 16));
-        autoRoute.add(new RobotPosition(15, 15));
-        autoRoute.add(new RobotPosition(14, 14));
-        autoRoute.add(new RobotPosition(13, 13));
-        autoRoute.add(new RobotPosition(12, 12));
+//        autoRoute.add(new RobotPosition(12, 12, 45));
+//        autoRoute.add(new RobotPosition(13, 12));
+//        autoRoute.add(new RobotPosition(14, 12));
+//        autoRoute.add(new RobotPosition(15, 12));
+//        autoRoute.add(new RobotPosition(16, 12));
+//        autoRoute.add(new RobotPosition(17, 12));
+//        autoRoute.add(new RobotPosition(18, 12));
+//        autoRoute.add(new RobotPosition(19, 12));
+//        autoRoute.add(new RobotPosition(20, 12));
+//        autoRoute.add(new RobotPosition(20, 13));
+//        autoRoute.add(new RobotPosition(20, 14));
+//        autoRoute.add(new RobotPosition(20, 15));
+//        autoRoute.add(new RobotPosition(20, 16));
+//        autoRoute.add(new RobotPosition(20, 17));
+//        autoRoute.add(new RobotPosition(20, 18));
+//        autoRoute.add(new RobotPosition(20, 19));
+//        autoRoute.add(new RobotPosition(20, 20));
+//        autoRoute.add(new RobotPosition(19, 19));
+//        autoRoute.add(new RobotPosition(18, 18));
+//        autoRoute.add(new RobotPosition(17, 17));
+//        autoRoute.add(new RobotPosition(16, 16));
+//        autoRoute.add(new RobotPosition(15, 15));
+//        autoRoute.add(new RobotPosition(14, 14));
+//        autoRoute.add(new RobotPosition(13, 13));
+//        autoRoute.add(new RobotPosition(12, 12));
+
+        Pathfinder pathfinder1 = new Pathfinder(32,0,1,-25);
+
+        autoRoute = pathfinder1.generateRobotPath();
 
         generateMoveActions(autoRoute);
 
