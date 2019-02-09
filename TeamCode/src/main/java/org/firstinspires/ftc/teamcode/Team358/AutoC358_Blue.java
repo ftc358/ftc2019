38f0e00d72c327d25cba2ccb688ecc26d3f111fa
Calvin Xu <1020806611@qq.com>
2019-02-08 20:42:14 -0800

    copy from red side

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team358/AutoC358_Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team358/AutoC358_Blue.java
@@ -11,14 +11,16 @@ public class AutoC358_Blue extends AutoEngine358 {
 
     public void runOpMode() throws InterruptedException {
 
-        //TODO: change this! actually unlatching from lander should be 45
-        initialize(new RobotPosition(-11, 11, 45));
+        initialize(new RobotPosition(11, -11, 225));
+
+        token.setPosition(0);
+        latch.setPower(0);
 
         waitForStart();
 
         while (opModeIsActive() && !done) {
 
-            //TODO: unlatch & go to starting position
+            unlatch();
 
             int cubePosition = lookForwardAndCheck();
 
@@ -26,44 +28,43 @@ public class AutoC358_Blue extends AutoEngine358 {
             telemetry.addData("current absolute heading", getAbsoluteCurrentHeading());
             telemetry.update();
 
-            //TODO: change headings
-
             switch (cubePosition) {
                 case 1:
-                    turn(new IMUTurner(calculateTurn(getAbsoluteCurrentHeading(),90), POWER, _imu1, 1, null), true, true);
+                    turnTo(90, POWER);
                     forward(POWER, 28);
-                    turn(new IMUTurner(calculateTurn(getAbsoluteCurrentHeading(),45), POWER, _imu1, 1, null), true, true);
-                    forward(POWER,7*sqrt(2));
-                    turn(new IMUTurner(calculateTurn(getAbsoluteCurrentHeading(),180), POWER, _imu1, 1, null), true, true);
-                    forward(POWER,-60);
-                    turn(new IMUTurner(calculateTurn(getAbsoluteCurrentHeading(),180), POWER, _imu1, 1, null), true, true);
+                    turnTo(45, POWER);
+                    forward(POWER, 7 * 2 * sqrt(2));
+                    turnTo(180, POWER);
+                    forward(POWER, -60);
+                    turnTo(180, POWER);
                     dropToken();
-                    forward(POWER,76);
+                    forward(POWER, 76);
                     done = true;
                     break;
                 case 2:
-                    turn(new IMUTurner(calculateTurn(getAbsoluteCurrentHeading(),135), POWER, _imu1, 1, null), true, true);
-                    forward(POWER,7*sqrt(2));
-                    forward(POWER,-4*sqrt(2));
-                    turn(new IMUTurner(calculateTurn(getAbsoluteCurrentHeading(),45), POWER, _imu1, 1, null), true, true);
-                    forward(POWER,18*sqrt(2));
-                    turn(new IMUTurner(calculateTurn(getAbsoluteCurrentHeading(),180), POWER, _imu1, 1, null), true, true);
-                    forward(POWER,44);
-                    turn(new IMUTurner(calculateTurn(getAbsoluteCurrentHeading(),180), POWER, _imu1, 1, null), true, true);
+                    turnTo(135, POWER);
+                    forward(POWER, 6 * 2 * sqrt(2));
+                    forward(POWER, -4 * 2 * sqrt(2));
+                    turnTo(45, POWER);
+                    forward(POWER, 18 * 2 * sqrt(2));
+                    turnTo(180, POWER);
+                    forward(POWER, -44);
+                    turnTo(180, POWER);
                     dropToken();
-                    forward(POWER,76);
+                    forward(POWER, 76);
                     done = true;
                     break;
                 case 3:
-                    turn(new IMUTurner(calculateTurn(getAbsoluteCurrentHeading(),180), POWER, _imu1, 1, null), true, true);
-                    forward(POWER,12);
-                    turn(new IMUTurner(calculateTurn(getAbsoluteCurrentHeading(),45), POWER, _imu1, 1, null), true, true);
-                    forward(POWER,21*sqrt(2));
-                    turn(new IMUTurner(calculateTurn(getAbsoluteCurrentHeading(),180), POWER, _imu1, 1, null), true, true);
-                    forward(POWER,44);
-                    turn(new IMUTurner(calculateTurn(getAbsoluteCurrentHeading(),180), POWER, _imu1, 1, null), true, true);
+                    turnTo(180, POWER);
+                    forward(POWER, 24);
+                    forward(POWER, -12);
+                    turnTo(45, POWER);
+                    forward(POWER, 21 * 2 * sqrt(2));
+                    turnTo(180, POWER);
+                    forward(POWER, -44);
+                    turnTo(180, POWER);
                     dropToken();
-                    forward(POWER,76);
+                    forward(POWER, 76);
                     done = true;
                     break;
             }
@@ -71,5 +72,15 @@ public class AutoC358_Blue extends AutoEngine358 {
     }
 
     public void dropToken() {
+        token.setPosition(0.6);
+    }
+
+    public void unlatch() throws InterruptedException {
+        runMotor(latch, 1, -10400);
+        forward(1, 2);
+        strafe(1, 12.5);
+        turnTo(225, POWER);
+        forward(1, -5.5);
+        done = true;
     }
 }
